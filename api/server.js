ccf8742fb06b995688313457b5a18ca146abc4c7
LucasRocha2308 <lucas_rocha_25@hotmail.com>
2024-11-02 22:30:45 -0300

    feat: adjustments logic server.js

--- api/server.js
+++ api/server.js
@@ -1,31 +1,28 @@
-// See https://github.com/typicode/json-server#module
-const jsonServer = require('json-server')
+const jsonServer = require("json-server");
+const path = require("path");
+const fs = require("fs");
 
-const server = jsonServer.create()
+const server = jsonServer.create();
+const middlewares = jsonServer.defaults();
+const filePath = path.join(__dirname, "..", "db.json");
+const data = fs.readFileSync(filePath, "utf-8");
+const db = JSON.parse(data);
+const router = jsonServer.router(db);
 
-// Uncomment to allow write operations
-// const fs = require('fs')
-// const path = require('path')
-// const filePath = path.join('db.json')
-// const data = fs.readFileSync(filePath, "utf-8");
-// const db = JSON.parse(data);
-// const router = jsonServer.router(db)
+server.use(middlewares);
 
-// Comment out to allow write operations
-const router = jsonServer.router('db.json')
+// Rewriter to support custom routes
+server.use(
+  jsonServer.rewriter({
+    "/api/*": "/$1",
+    "/blog/:resource/:id/show": "/:resource/:id",
+  })
+);
 
-const middlewares = jsonServer.defaults()
+server.use(router);
 
-server.use(middlewares)
-// Add this before server.use(router)
-server.use(jsonServer.rewriter({
-    '/api/*': '/$1',
-    '/blog/:resource/:id/show': '/:resource/:id'
-}))
-server.use(router)
 server.listen(3000, () => {
-    console.log('JSON Server is running')
-})
+  console.log("JSON Server is running");
+});
 
-// Export the Server API
-module.exports = server
+module.exports = server;
